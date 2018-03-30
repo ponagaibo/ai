b2342abc92315c19d05906d01855f46e281b6801
ponagaibo <ponagaibo@mail.ru>
2018-03-30 23:16:10 +0300

    add r-squared; quit exit ; l

--- lab2.py
+++ lab2.py
@@ -1,25 +1,43 @@
-import quandl
+import quandl, math
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import preprocessing, cross_validation
 
 df = quandl.get('Wiki/GOOGL')
 df.to_csv("GOOGL.csv")
-df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
+df = df[['Open', 'High', 'Low', 'Close']]
+
+forecast_col = 'Close'
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
 
 df = df.reset_index()
-df['Num'] = df.index
-df = df[['Num', 'Open', 'High', 'Low', 'Close', 'Volume']]
-print(df.head())
+df = df[['Open', 'High', 'Low', 'Close', 'label']]
 
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 model = LinearRegression()
+model.fit(X_train, y_train)
+print("R-squared: ")
+print(model.score(X_test, y_test))
+
 X = df[['Open']].values
 y = df['Close'].values
 model.fit(X, y)
+
 plt.figure()
 plt.xlabel('Open')
 plt.ylabel('Close')
 plt.plot(X, y, 'k.')
-plt.plot(X, model.predict(X), color='g')
 
-plt.grid(True)
+model.fit(X, y)
+plt.plot(X, model.predict(X), color='g')
 plt.show()
